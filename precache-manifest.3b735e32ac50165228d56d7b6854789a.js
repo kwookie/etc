194323eed37769475e450e24c0b6579a77f226e1
Karsten Wokel <karsten@KBook-Air.local>
2025-04-15 20:18:03 +0100

    Updates

---
+++ precache-manifest.3b735e32ac50165228d56d7b6854789a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a006929c54c1e1a6e465526032594e9",
+    "url": "/etc/index.html"
+  },
+  {
+    "revision": "7ebbfddf140d8918cba7",
+    "url": "/etc/static/css/2.826699c9.chunk.css"
+  },
+  {
+    "revision": "45fca153b2d9753d32a0",
+    "url": "/etc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7ebbfddf140d8918cba7",
+    "url": "/etc/static/js/2.99b0cee0.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/etc/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "45fca153b2d9753d32a0",
+    "url": "/etc/static/js/main.791b9779.chunk.js"
+  },
+  {
+    "revision": "f3f52ae71dfa961e875b",
+    "url": "/etc/static/js/runtime~main.5e06f19c.js"
+  }
+]);
